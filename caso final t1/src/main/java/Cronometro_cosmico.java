0b9674924939f772e2d34d251645b1e81a783b35
ZoeAcero <zar.amazona@gmail.com>
2024-02-21 11:17:03 +0100

    nuevo

--- caso final t1/src/main/java/Cronometro_cosmico.java
+++ caso final t1/src/main/java/Cronometro_cosmico.java
@@ -2,4 +2,7 @@ import java.util.Scanner;
 public class Cronometro_cosmico {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Bienvenido al Cronómetro Cósmico");
+        System.out.println("Ingrese la duración del día en el nuevo planeta (en horas): ");
+        double duracionDiaPlaneta = scanner.nextDouble();
 }
