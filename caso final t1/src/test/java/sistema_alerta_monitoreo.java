c18d46f8c054b7e423286ada91dadc9655bf0a39
ZoeAcero <zar.amazona@gmail.com>
2024-02-21 11:46:18 +0100

    nuevo

--- caso final t1/src/test/java/sistema_alerta_monitoreo.java
+++ caso final t1/src/test/java/sistema_alerta_monitoreo.java
@@ -32,4 +32,21 @@ public class sistema_alerta_monitoreo {
         }
     }
 
+    private static int generarNumeroAleatorio(int min, int max) {
+        return (int) (Math.random() * (max - min + 1) + min);
+    }
+
+    private static boolean esNumeroPrimo(int numero) {
+        if (numero <= 1) {
+            return false;
         }
+        for (int i = 2; i <= Math.sqrt(numero); i++) {
+            if (numero % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+
