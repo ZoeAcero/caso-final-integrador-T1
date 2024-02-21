cd2b05273284036fb29a615f09b66cb896164617
ZoeAcero <zar.amazona@gmail.com>
2024-02-21 11:36:01 +0100

    nuevo

---
+++ caso final t1/src/test/java/recursos_suministros.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class recursos_suministros {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Bienvenido al Gestor de Recursos");
+}
