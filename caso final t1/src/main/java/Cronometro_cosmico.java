8622d8005e90011b2ac8563113b040aaab0c6e81
ZoeAcero <zar.amazona@gmail.com>
2024-02-21 11:17:36 +0100

    nuevo

--- caso final t1/src/main/java/Cronometro_cosmico.java
+++ caso final t1/src/main/java/Cronometro_cosmico.java
@@ -9,4 +9,7 @@ public class Cronometro_cosmico {
         System.out.println("Ingrese la duración del año en el nuevo planeta (en días): ");
         double duracionAnioPlaneta = scanner.nextDouble();
 
+        System.out.println("Ingrese el tiempo en la Tierra (en días): ");
+        double tiempoTierra = scanner.nextDouble();
+
     }
