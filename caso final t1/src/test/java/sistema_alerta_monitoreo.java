bd231d008a59e0d5cdd7b8fffd8372f8ef4b849e
ZoeAcero <zar.amazona@gmail.com>
2024-02-21 11:44:54 +0100

    nuevo

--- caso final t1/src/test/java/sistema_alerta_monitoreo.java
+++ caso final t1/src/test/java/sistema_alerta_monitoreo.java
@@ -15,4 +15,21 @@ public class sistema_alerta_monitoreo {
         while (true) {
             double temperatura = generarNumeroAleatorio(10, 40);
             int nivelCO2 = generarNumeroAleatorio(200, 1000);
-}
+
+            if (esNumeroPrimo(nivelCO2)) {
+                System.out.println("¡Alerta! Nivel de CO2 inusualmente alto: " + nivelCO2);
+            }
+
+            if (temperatura > 35) {
+                System.out.println("¡Alerta! Temperatura críticamente alta: " + temperatura);
+            }
+
+            try {
+                Thread.sleep(5000);  // Pausa de 5 segundos entre monitoreos
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+        }
