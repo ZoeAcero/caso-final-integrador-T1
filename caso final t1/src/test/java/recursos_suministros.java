724e47219c26681ba71e359d3cd8e6508ada94a2
ZoeAcero <zar.amazona@gmail.com>
2024-02-21 11:36:41 +0100

    nuevo

--- caso final t1/src/test/java/recursos_suministros.java
+++ caso final t1/src/test/java/recursos_suministros.java
@@ -5,4 +5,9 @@ public class recursos_suministros {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Bienvenido al Gestor de Recursos");
+        System.out.println("Ingrese el nivel inicial de agua (en litros): ");
+        double nivelAgua = scanner.nextDouble();
+
+        System.out.println("Ingrese el nivel inicial de alimentos (en kilogramos): ");
+        double nivelAlimentos = scanner.nextDouble();
 }
